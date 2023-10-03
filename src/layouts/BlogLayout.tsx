ab52e8179d0bc1a33f054bfa181d1c41cfa45f20
DJ Corbett <dcorbett@yext.com>
2023-10-03 15:10:25 -0400

    updating layout components

--- src/layouts/BlogLayout.tsx
+++ src/layouts/BlogLayout.tsx
@@ -1,15 +1,15 @@
-import Main from "../components/atoms/MainContentWrapper";
-import Page from "../components/atoms/PageWrapper";
+import MainContentWrapper from "../components/atoms/MainContentWrapper";
+import PageWrapper from "../components/atoms/PageWrapper";
 import Footer from "../components/molecules/Footer";
 import Header from "../components/molecules/Header";
 
 const BlogLayout = () => {
   return (
-    <Page>
+    <PageWrapper>
       <Header logo="http://a.mktgcdn.com/p/86Moa_TLbLDstVl9pCx-CZwrroZevu43XtPiCZVCG3U/300x300.png" />
-      <Main></Main>
+      <MainContentWrapper></MainContentWrapper>
       <Footer />
-    </Page>
+    </PageWrapper>
   );
 };
 
